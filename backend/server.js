9cb1602fdc5f1161faf02073794f1c42d0acbcc7
Jimmy Mathew <jimmymathew765@gmail.com>
2025-02-15 20:33:16 +0530

    Update server.js comment

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const cors = require('cors');
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: 'https://mytest-front.vercel.app', // Allow requests from your frontend
+  origin: 'https://mytest-front.vercel.app', // Allow requests from your frontend man
 
 }));
 
