e3bceb6d0942a2d3f0b97e6ad48c4805714a132e
Jimmy Mathew <jimmymathew765@gmail.com>
2025-02-15 20:07:24 +0530

    Update LoginPage.jsx

--- frontend/immunoconnect/src/pages/LoginPage.jsx
+++ frontend/immunoconnect/src/pages/LoginPage.jsx
@@ -15,7 +15,7 @@ const LoginPage = () => {
       // Clear existing localStorage data
       localStorage.clear();
 
-      const response = await fetch("http://localhost:5000/api/users/login", {
+      const response = await fetch("https://mytest-murex-kappa.vercel.app/api/users/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
