ba10c373238dd836a495e143bba9163013277bcb
Jimmy Mathew <jimmymathew765@gmail.com>
2025-02-16 13:38:40 +0530

    Update ScheduleTab.jsx

--- frontend/immunoconnect/src/pages/components/Dashboard/tabs/ScheduleTab.jsx
+++ frontend/immunoconnect/src/pages/components/Dashboard/tabs/ScheduleTab.jsx
@@ -10,7 +10,7 @@ const ScheduleTab = () => {
   useEffect(() => {
     const fetchChildren = async () => {
       try {
-        const response = await axios.get("https://mytest-murex-kappa.vercel.app/api/children", {
+        const response = await axios.get("https://mytest-kk5g.onrender.com/api/children", {
           headers: {
             Authorization: `Bearer ${localStorage.getItem("token")}`,
           },
