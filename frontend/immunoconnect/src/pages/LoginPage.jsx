d459647054fd9a66bea702df998694ea79b423d1
Jimmy007Mathew <jimmymathew765@gmail.com>
2025-02-17 08:18:32 +0530

    new

--- frontend/immunoconnect/src/pages/LoginPage.jsx
+++ frontend/immunoconnect/src/pages/LoginPage.jsx
@@ -16,7 +16,7 @@ const LoginPage = () => {
       localStorage.clear();
 
       const response = await fetch(
-        "https://immunobackend.vercel.app/api/users/login",
+        "https://mytest-neon.vercel.app/api/users/login",
         {
           method: "POST",
           headers: {
