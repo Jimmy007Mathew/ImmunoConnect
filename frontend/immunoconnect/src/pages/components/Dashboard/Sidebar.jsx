b75598092eb2f91f50a09af11c0beb1daf805b71
Jimmy007Mathew <jimmymathew765@gmail.com>
2025-02-19 23:20:20 +0530

    sidebar

--- frontend/immunoconnect/src/pages/components/Dashboard/Sidebar.jsx
+++ frontend/immunoconnect/src/pages/components/Dashboard/Sidebar.jsx
@@ -23,8 +23,7 @@ const Sidebar = ({
     { icon: Home, label: "Dashboard", id: "overview" },
     { icon: Users, label: "Family Profiles", id: "profiles" },
     { icon: Calendar, label: "Schedule", id: "schedule" },
-    { icon: FileText, label: "Records", id: "records" },
-    { icon: BarChart, label: "Analytics", id: "analytics" },
+    { icon: FileText, label: "Vaccination Card", id: "records" },
     { icon: MapPin, label: "Health Centers", id: "centers" },
   ];
 
@@ -33,12 +32,17 @@ const Sidebar = ({
       className={`${
         isSidebarOpen ? "translate-x-0" : "-translate-x-full"
       } fixed inset-y-0 left-0 z-50 w-72 bg-white shadow-lg transform transition-transform duration-300 ease-in-out lg:translate-x-0 lg:static lg:inset-0`}
+      style={{
+        position: "fixed", // Ensure sidebar is fixed on desktop
+        height: "100vh", // Full height
+        overflowY: "auto", // Allow scrolling only for the sidebar
+      }}
     >
       <div className="h-full flex flex-col">
-        <div className="p-4 border-b">
+        <div className="p-4 pt-5 border-b-2">
           <div className="flex items-center justify-between">
             <span className="text-xl font-bold text-blue-600 p-1 flex">
-              <Shield className="h-6 w-6  text-blue-600 mr-3" />
+              <Shield className="h-6 w-6 text-blue-600 mr-3" />
               ImmunoConnect
             </span>
             <button
